63451236f3d64febcc5f44afdc8f24abad124f82
Vrigz Alejo <nuedu2012@gmail.com>
2015-11-18 21:15:25 +0800

    5

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,16 +3,18 @@
  */
 // Backbone Model
 
-var Blog = Backbone.Model.extend({
-    defaults: {
-        author: '',
-        title: '',
-        url: ''
-    }
-});
+var Blog = Backbone.Model.extend( {
+	defaults: {
+		author: '',
+		title: '',
+		url: ''
+	}
+} );
 
 // Backbone Collection
-var Blogs = Backbone.Collection.extend({});
+var Blogs = Backbone.Collection.extend( {
+	url: 'http://localhost:3000/api/blogs'
+} );
 
 /*var blog1 = new Blog({
  author: 'Vrigz',
@@ -35,84 +37,104 @@ var blogs = new Blogs();
 
 
 // Backbone View for one blog
-var BlogView = Backbone.View.extend({
-    model: new Blog(),
-    tagName: 'tr',
-    initialize: function() {
-        this.template = _.template($('.blogs-list-template').html());
-    },
-    events: {
-        'click .edit-blog': 'edit',
-        'click .update-blog': 'update',
-        'click .cancel': 'cancel',
-        'click .delete-blog': 'delete'
-    },
-    edit: function() {
-        $('.edit-blog').hide();
-        $('.delete-blog').hide();
-        this.$('.update-blog').show();
-        this.$('.cancel').show();
+var BlogView = Backbone.View.extend( {
+	model: new Blog(),
+	tagName: 'tr',
+	initialize: function() {
+		this.template = _.template( $( '.blogs-list-template' ).html() );
+	},
+	events: {
+		'click .edit-blog': 'edit',
+		'click .update-blog': 'update',
+		'click .cancel': 'cancel',
+		'click .delete-blog': 'delete'
+	},
+	edit: function() {
+		$( '.edit-blog' ).hide();
+		$( '.delete-blog' ).hide();
+		this.$( '.update-blog' ).show();
+		this.$( '.cancel' ).show();
 
-        var author = this.$('.author').html();
-        var title = this.$('.title').html();
-        var url = this.$('.url').html();
+		var author = this.$( '.author' ).html();
+		var title = this.$( '.title' ).html();
+		var url = this.$( '.url' ).html();
 
-        this.$('.author').html('<input type="text" class="form-control author-update" value="' + author + '">');
-        this.$('.title').html('<input type="text" class="form-control title-update" value="' + title + '">');
-        this.$('.url').html('<input type="text" class="form-control url-update" value="' + url + '">');
+		this.$( '.author' ).html( '<input type="text" class="form-control author-update" value="' + author + '">' );
+		this.$( '.title' ).html( '<input type="text" class="form-control title-update" value="' + title + '">' );
+		this.$( '.url' ).html( '<input type="text" class="form-control url-update" value="' + url + '">' );
 
-    },
-    update: function() {
-        this.model.set('author', $('.author-update').val());
-        this.model.set('title', $('.title-update').val());
-        this.model.set('url', $('.url-update').val());
-    },
-    cancel: function() {
-        blogsView.render();
-    },
-    delete: function() {
-        this.model.destroy();
-    },
-    render: function() {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
-    }
-});
+	},
+	update: function() {
+		this.model.set( 'author', $( '.author-update' ).val() );
+		this.model.set( 'title', $( '.title-update' ).val() );
+		this.model.set( 'url', $( '.url-update' ).val() );
+	},
+	cancel: function() {
+		blogsView.render();
+	},
+	delete: function() {
+		this.model.destroy();
+	},
+	render: function() {
+		this.$el.html( this.template( this.model.toJSON() ) );
+		return this;
+	}
+} );
 
 // Backbone View for all blogs
-var BlogsView = Backbone.View.extend({
-    model: blogs,
-    el: $('.blogs-list'),
-    initialize: function() {
-        var self = this;
-        this.model.on('add', this.render, this);
-        this.model.on('change', function() {
-            setTimeout(function() {
-                self.render();
-            }, 30);
-        }, this);
-        this.model.on('remove', this.render, this);
-    },
-    render: function() {
-        var self = this;
-        this.$el.html('');
-        _.each(this.model.toArray(), function(blog) {
-            self.$el.append((new BlogView({ model: blog })).render().$el);
-        });
-        return this;
-    }
-});
+var BlogsView = Backbone.View.extend( {
+	model: blogs,
+	el: $( '.blogs-list' ),
+	initialize: function() {
+		var self = this;
+		this.model.on( 'add', this.render, this );
+		this.model.on( 'change', function() {
+			setTimeout( function() {
+				self.render();
+			}, 30 );
+		}, this );
+		this.model.on( 'remove', this.render, this );
+
+		this.model.fetch( {
+			success: function( response ) {
+				_.each( response, function( item ) {
+					console.log( 'Successfully GOT blog with _id: ' + item._id );
+				} );
+			},
+			error: function() {
+				console.log( 'Failed to get blogs!' );
+			}
+		} );
+	},
+	render: function() {
+		var self = this;
+		this.$el.html( '' );
+		_.each( this.model.toArray(), function( blog ) {
+			self.$el.append( (new BlogView( { model: blog } )).render().$el );
+		} );
+		return this;
+	}
+} );
 
 var blogsView = new BlogsView();
 
-$(document).ready(function() {
-    $('.add-blog').on('click', function() {
-        var blog = new Blog({
-            author: $('.author-input').val(),
-            title: $('.title-input').val(),
-            url: $('.url-input').val()
-        });
-        console.log(blog.toJSON());
-        blogs.add(blog);
-    });
-});
+$( document ).ready( function() {
+	$( '.add-blog' ).on( 'click', function() {
+		var blog = new Blog( {
+			author: $( '.author-input' ).val(),
+			title: $( '.title-input' ).val(),
+			url: $( '.url-input' ).val()
+		} );
+		//console.log( blog.toJSON() );
+		blogs.add( blog );
+
+		blog.save(null, {
+			success: function(response) {
+				console.log('Successfully SAVED blog with _id: ' + response.toJSON()._id);
+			},
+			error: function() {
+				console.log('Failed to save blog!');
+			}
+		})
+	} );
+} );
--- server.js
+++ server.js
@@ -2,37 +2,57 @@
  * Created by vrigzlinuxmint13 on 11/18/15.
  */
 var express = require( 'express' );
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/blogroll');
-
+var bodyParser = require( 'body-parser' );
+var mongoose = require( 'mongoose' );
+var app = express();
+mongoose.connect( 'mongodb://localhost/blogroll' );
 var Schema = mongoose.Schema;
-
-var BlogSchema = new Schema({
+var BlogSchema = new Schema( {
 	author: String,
 	title: String,
 	url: String
-});
-
-mongoose.model('Blog', BlogSchema);
-
-var Blog = mongoose.model('Blog');
-
-var blog = new Blog({
+} );
+mongoose.model( 'Blog', BlogSchema );
+var Blog = mongoose.model( 'Blog' );
+var blog = new Blog( {
 	author: 'Vrigz',
 	title: 'Vrigz\'s Blog',
 	url: 'http://www.brigidoantazoalejo.space'
-});
+} );
+var port = 3000;
 
 blog.save();
 
-var app = express();
-
-
+// ROOT
 app.use( express.static( __dirname + '/public' ) );
 
-var port = 3000;
+// PARSER
+app.use( bodyParser.json() );
 
+// ROUTES
+app.get( '/api/blogs', function( req, res ) {
+	Blog.find( function( err, docs ) {
+		docs.forEach( function( item ) {
+			console.log( 'Received a GET request for _id: ' + item._id );
+		} );
+		res.send( docs );
+	} );
+} );
+
+app.post( '/api/blogs', function( req, res ) {
+
+	console.log( 'Received a POST request' );
+	for( var key in req.body ) {
+		console.log( key + ': ' + req.body[ key ] );
+	}
+	var blog = new Blog( req.body );
+	blog.save( function( err, doc ) {
+		res.send( doc );
+	} )
+} );
+
+// PORT 3000
 app.listen( port );
 
+// CONSOLE MESSAGE
 console.log( 'server on ' + port );
